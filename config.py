b052b5ec079e27de62e97cfa6b01b07675656804
Aethor <oow.autre@gmail.com>
2019-12-13 15:06:35 +0800

    Reverted to old config scheme

--- config.py
+++ config.py
@@ -1,25 +1,22 @@
-import argparse
-import re
 import json
 
 
 class Config:
-    def __init__(self, definition_path: str):
-        arg_parser = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter
-        )
-        with open(definition_path) as definition_file:
-            definition = json.loads(definition_file.read())
-            for key, item in definition.items():
-                arg_parser.add_argument(
-                    "--" + re.sub("_", "-", key),
-                    type=eval(item["type"]),
-                    default=item["default"],
-                    help=item["help"],
-                )
+    def __init__(self, filename: str):
+        self.load_from_file_(filename)
 
-        args = arg_parser.parse_args()
-        self.config = vars(args)
+    def load_from_file_(self, filename: str):
+        file = open(filename)
+        self.config = json.loads(file.read())
+        file.close()
 
-    def __getitem__(self, key):
+    def update_(self, config_dict: dict):
+        for key in config_dict.keys():
+            if key in self.config:
+                self.config[key] = config_dict[key]
+
+    def __getitem__(self, key: str):
         return self.config[key]
+
+    def __str__(self):
+        return json.dumps(self.config, indent=4)
