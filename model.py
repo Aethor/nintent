8391f15ab379fd6f4977fc555d4a4a1a838aba78
Aethor <oow.autre@gmail.com>
2020-01-03 11:41:38 +0800

    Enhanced training

--- model.py
+++ model.py
@@ -7,6 +7,12 @@ from transformers import BertModel, BertTokenizer
 from tree import IntentTree, Intent, Slot
 
 
+def are_spans_overlapping(span1, span2):
+    return (span1[0] >= span2[0] and span1[0] < span2[1]) or (
+        span1[1] > span2[0] and span1[1] <= span2[1]
+    )
+
+
 class SpanEncoder(torch.nn.Module):
     def __init__(self):
         super(SpanEncoder, self).__init__()
@@ -19,21 +25,6 @@ class SpanEncoder(torch.nn.Module):
         :return:     (batch_size, seq_size, hidden_size)
         """
         return self.bert_encoder(seqs)[0]
-
-
-class SpanScorer(torch.nn.Module):
-    def __init__(self, hidden_size: int):
-        super(SpanScorer, self).__init__()
-        self.scorer = torch.nn.Linear(2 * hidden_size, 1)
-
-    def forward(self, span_repr: torch.Tensor) -> torch.Tensor:
-        """
-        :param span_repr: (batch_size, 2, hidden_size)
-        :return:          (batch_size, 1)
-        """
-        # (batch_size, spans_nb, 2 * hidden_size)
-        flat_spans = torch.flatten(span_repr, start_dim=1).float()
-        return torch.sigmoid(self.scorer(flat_spans))
 
 
 class Selector(torch.nn.Module):
@@ -59,8 +50,6 @@ class TreeScorer(torch.nn.Module):
 
         self.span_encoder = SpanEncoder()
         self.hidden_size = self.span_encoder.hidden_size
-
-        self.span_scorer = SpanScorer(self.hidden_size)
 
         self.is_slot_selector = Selector(self.hidden_size, 2)
         self.is_intent_selector = Selector(self.hidden_size, 2)
@@ -103,32 +92,43 @@ class TreeScorer(torch.nn.Module):
                 [0 + coords_offset, coords_offset + len(tokens_str)],
             )
 
+            selected_spans: List[Tuple[Tuple[int], float]] = list()
             for span_size in range(tokens_repr.shape[1] - 1, 0, -1):
                 for span_start in range(0, tokens_repr.shape[1] - span_size + 1):
                     span_end = span_start + span_size
-
-                    is_overlapping = False
-                    for child_span in cur_tree.children_spans():
-                        if span_start >= child_span[0] and span_start < child_span[1]:
-                            is_overlapping = True
-                        if span_end > child_span[0] and span_end <= child_span[1]:
-                            is_overlapping = True
-                    if is_overlapping:
-                        continue
 
                     span_repr = self.span_repr(tokens_repr[:, span_start:span_end, :])
                     is_slot_pred = self.is_slot_selector(span_repr)[0]
                     is_slot = torch.max(is_slot_pred, 0).indices.item() == 1
 
                     if is_slot:
-                        cur_tree.add_child_(
-                            self.make_tree(
-                                tokens_str[span_start:span_end],
-                                device,
-                                Slot,
-                                coords_offset + span_start,
+                        overlapping_spans = list()
+                        for span_coords, span_score in selected_spans:
+                            if are_spans_overlapping(
+                                (span_start, span_end), span_coords
+                            ):
+                                overlapping_spans.append((span_coords, span_score))
+                        if all(
+                            [
+                                is_slot_pred[1] > overlapping_span[1]
+                                for overlapping_span in overlapping_spans
+                            ]
+                        ):
+                            for overlapping_span in overlapping_spans:
+                                selected_spans.remove(overlapping_span)
+                            selected_spans.append(
+                                ((span_start, span_end), is_slot_pred[1])
                             )
-                        )
+
+            for selected_span in selected_spans:
+                cur_tree.add_child_(
+                    self.make_tree(
+                        tokens_str[selected_span[0][0] : selected_span[0][1]],
+                        device,
+                        Slot,
+                        coords_offset + selected_span[0][0],
+                    )
+                )
 
         elif cur_type == Slot:
             slot_type = Slot.itos(
@@ -182,7 +182,11 @@ class TreeScorer(torch.nn.Module):
             )
 
             candidate_span_nb = sum(range(2, tokens_repr.shape[1] + 1))
+            # FIXME: experimental weight
+            if len(gold_tree.children_spans()) > 0:
+                negative_weight = candidate_span_nb / len(gold_tree.children_spans())
             span_loss = torch.tensor([0], dtype=torch.float).to(device)
+            selected_spans: List[Tuple[Tuple[int], float]] = list()
             for span_size in range(tokens_repr.shape[1] - 1, 0, -1):
                 for span_start in range(0, tokens_repr.shape[1] - span_size + 1):
                     span_end = span_start + span_size
@@ -191,14 +195,30 @@ class TreeScorer(torch.nn.Module):
                     is_slot_pred = self.is_slot_selector(span_repr)[0]
                     is_slot = torch.max(is_slot_pred, 0).indices.item() == 1
 
-                if [span_start, span_end] in gold_tree.children_spans():
-                    # FIXME: experimental weight
-                    negative_weight = candidate_span_nb / len(
-                        gold_tree.children_spans()
-                    )
-                    span_loss += negative_weight * is_slot_pred[0]
+                    if is_slot:
+                        overlapping_spans = list()
+                        for span_coords, span_score in selected_spans:
+                            if are_spans_overlapping(
+                                (span_start, span_end), span_coords
+                            ):
+                                overlapping_spans.append((span_coords, span_score))
+                        if all(
+                            [
+                                is_slot_pred[1] > overlapping_span[1]
+                                for overlapping_span in overlapping_spans
+                            ]
+                        ):
+                            for overlapping_span in overlapping_spans:
+                                selected_spans.remove(overlapping_span)
+                            selected_spans.append(
+                                ((span_start, span_end), is_slot_pred[1])
+                            )
+
+            for span in selected_spans:
+                if list(span[0]) in gold_tree.children_spans():
+                    span_loss += negative_weight * (1 - span[1])
                 else:
-                    span_loss += is_slot_pred[1]
+                    span_loss += span[1]
             if candidate_span_nb > 0:
                 loss += span_loss / candidate_span_nb
 
@@ -217,12 +237,9 @@ class TreeScorer(torch.nn.Module):
             is_intent_pred = self.is_intent_selector(self.span_repr(tokens_repr))[0]
             is_intent = torch.max(is_intent_pred, 0).indices.item() == 1
             if gold_tree.is_leaf():
-                loss += is_intent_pred[0]
-            else:
                 loss += is_intent_pred[1]
-
-            # FIXME:
-            assert len(gold_tree.children) <= 1
+            else:
+                loss += is_intent_pred[0]
 
             if gold_tree.is_leaf():
                 return loss
